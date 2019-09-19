6795d9a328b5cc9044007025354e1717656e0ee8
Matt DesEnfants <v-matdes@microsoft.com>
2019-09-19 11:07:19 -0700

    satisfactory vector

--- vectorizer.py
+++ vectorizer.py
@@ -1,10 +1,13 @@
 import json
-from nltk.tokenize import RegexpTokenizer, sent_tokenize
 
+from nltk.tokenize import RegexpTokenizer, sent_tokenize
 from nltk.stem import PorterStemmer
 from nltk import WordNetLemmatizer
 from nltk import ngrams
 from nltk.corpus import stopwords
+
+import matplotlib.pyplot as plt
+import numpy as np 
 
 import re
 import string
@@ -27,7 +30,22 @@ with open('./products/8007.json') as document:
                 for w in t if w not in stop_words] for t in tokens]
     gram1 = [[ng for ng in ngrams(s, 1)] for s in stemmed]
     gram2 = [[ng for ng in ngrams(s, 2)] for s in stemmed]
-    gram3 = [[ng for ng in ngrams(s, 3)] for s in stemmed]
-    print([[s[0] for s in g] for g in gram1])
-    print([[" ".join(s) for s in g] for g in gram2])
-    #print([[" ".join(s) for s in g] for g in gram3])
+    # seems like overkill: gram3 = [[ng for ng in ngrams(s, 3)] for s in stemmed]
+    gram1_strings = [[s[0] for s in g] for g in gram1]
+    gram2_strings = [[" ".join(s) for s in g] for g in gram2]
+
+    # seems like overkill: print([[" ".join(s) for s in g] for g in gram3])
+
+    singles = [item for sublist in gram1_strings for item in sublist]
+    doubles = [item for sublist in gram2_strings for item in sublist]
+
+    combined = singles + doubles
+
+    sum_size = 20
+    hashes = [(hash(i) % sum_size, 1) for i in combined]
+
+    sums = [0 for i in range(0, sum_size)]
+    for h in hashes:
+        sums[h[0]] = sums[h[0]] + h[1]
+    
+    print(sums)
