2eca313de21ebdca963cb81e8433134861296274
Matt DesEnfants <v-matdes@microsoft.com>
2019-09-20 11:15:25 -0700

    pretty svgs

--- vectorizer.py
+++ vectorizer.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import re
+import os
 import string
 import glob
 from math import pi
@@ -21,6 +22,15 @@ lemma = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
 files = glob.glob('./products/*.json')
+
+try:
+    os.mkdir('./products')
+except:
+    pass
+
+sum_size = 20
+shapes = []
+maxes = [0 for i in range(0, sum_size)]
 
 for f in files:
     product_id = ntpath.basename(f).split('.')[0]
@@ -49,42 +59,45 @@ for f in files:
         doubles = [item for sublist in gram2_strings for item in sublist]
 
         combined = singles + doubles
-
-        sum_size = 20
         hashes = [(hash(i) % sum_size, 1) for i in combined]
 
         sums = [0 for i in range(0, sum_size)]
         for h in hashes:
             sums[h[0]] = sums[h[0]] + h[1]
 
-        print(sums)
+        shapes.append([product_id] + sums)
 
         N = len(sums)
+        maxes = [max(x[0], x[1]) for x in zip(sums, maxes)]
 
-        # We are going to plot the first line of the data frame.
-        # But we need to repeat the first value to close the circular graph:
-        values = sums
-        values += values[:1]
+for values in shapes:
+    print(values)
+    # We are going to plot the first line of the data frame.
+    # But we need to repeat the first value to close the circular graph:
+    product_id = values[0]
+    values = [x[0] / x[1] for x in zip(values[1:], maxes)]
+    values += values[:1]
 
-        angles = [n / float(N) * 2 * pi for n in range(N)]
-        angles += angles[:1]
+    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles += angles[:1]
 
-        ax = plt.subplot(111, polar=True)
+    plt.figure(figsize=(1,1))
+    ax = plt.subplot(111, polar=True)
 
-        plt.xticks(angles[:-1], angles, color='grey', size=8)
-        ax.set_rlabel_position(0)
-        max_value = max(values)
-        plt.yticks([max_value / 2.0, max_value],
-                   [max_value / 2.0, max_value], color="grey", size=7)
-        plt.ylim(0, max(values))
+    plt.xticks(angles[:-1], angles, color='grey', size=8)
+    ax.set_rlabel_position(0)
+    max_value = max(values)
+    plt.yticks([0.5, 1],
+               ['', ''], color="grey", size=7)
+    plt.ylim(0, max(values))
 
-        ax.plot(angles, values, linewidth=1, linestyle='solid')
+    ax.plot(angles, values, linewidth=1, linestyle='solid')
 
-        # Fill area
-        ax.fill(angles, values, 'b', alpha=0.1)
-        ax.set_thetagrids([a * 180/np.pi for a in angles],
-                          ["" for n in range(0, len(sums))])
+    # Fill area
+    ax.fill(angles, values, 'b', alpha=0.1)
+    ax.set_thetagrids([a * 180/np.pi for a in angles],
+                      ["" for n in range(0, len(sums))])
 
-        fig = plt.gcf()
-        fig.savefig('./products/'+product_id+'.png', dpi=fig.dpi)
-        plt.clf()
+    fig = plt.gcf()
+    fig.savefig('./products/images/'+product_id+'.svg', dpi=fig.dpi)
+    plt.clf()
